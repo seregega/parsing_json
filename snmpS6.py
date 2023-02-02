4b75118696a832425fa71d90158c45437bdeb2c4
tester <t.burenkina@gmail.com>
2023-02-02 18:06:44 +0300

    Parsing unreal

--- snmpS6.py
+++ snmpS6.py
@@ -7,6 +7,7 @@ def compare_snmpS6():
     r9 = requests.get('http://192.168.0.242/snmpS6.json')
     snmpS6 = r9.json()
     print('snmpS6')
+    print(snmpS6)
     param_snmpS6 = ['s-cron-name1', 's-cron-range1', 's-cron-date1', 's-cron-name2', 's-cron-range2', 's-cron-date2']
     with open("snmpS6.json", 'r') as test_data:
         receivesnmpS6 = json.loads(test_data.read())
